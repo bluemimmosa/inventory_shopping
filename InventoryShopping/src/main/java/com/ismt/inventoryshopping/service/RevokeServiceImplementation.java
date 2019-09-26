e17527977471a0d7de0fbdee63d6b40128159e62
Primax <console1913@gmail.com>
2019-09-26 21:59:03 +0545

    Transaction DAO and SERVICE remaining.

---
+++ InventoryShopping/src/main/java/com/ismt/inventoryshopping/service/RevokeServiceImplementation.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ismt.inventoryshopping.service;
+
+import com.ismt.inventoryshopping.dao.RevokeDAO;
+import com.ismt.inventoryshopping.entity.Revoke;
+import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ *
+ * @author Primax
+ */
+public class RevokeServiceImplementation implements RevokeService{
+    
+    @Autowired
+    RevokeDAO revokedao;
+    
+    @Override
+    public boolean revokeItem(Revoke revoke) {
+        return revokedao.revokeItem(revoke);
+    }
+
+    @Override
+    public ArrayList<Revoke> listAll() {
+        return revokedao.listAll();
+    }
+    
+}
